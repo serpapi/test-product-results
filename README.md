e76a2be34de9b380b682469e4b53af63d2cf586d
Michael Mooney <mikeyy@mikeyy.com>
2019-05-09 17:51:52 -0500

    Initial push for Product results test unit
    Unsure how to proceed with local sellers results, it's a hit or miss. Will come back to it

---
+++ README.md
@@ -0,0 +1,5 @@
+# Product Results
+
+[![Build Status](https://travis-ci.org/serpapi/test-product-results.svg?branch=master)](https://travis-ci.org/serpapi/test-product-results)
+
+Tests for SerpApi Product results
---
+++ spec/product_results_tv_spec.rb
@@ -0,0 +1,64 @@
+describe "SerpApi Product JSON" do
+
+  describe "Product results for Sceptre X505BV-FSR - 50 LED TV - 1080p" do
+
+    before :all do
+      @response = HTTP.get 'http://localhost:3000/search.json?engine=google_product&product=16618293882053925702&source=test'
+      @json = @response.parse
+    end
+
+    it "returns http success" do
+      expect(@response.code).to be(200)
+    end
+
+    it "contains Product results hash" do
+      expect(@json["product_results"]).to be_an(Hash)
+    end
+
+    describe "have correct Product results" do
+
+      before :all do
+        @result = @json["product_results"]
+      end
+
+      it "product is 16618293882053925702" do
+         expect(@result["product"]).to eql("16618293882053925702")
+      end
+  
+      it "title is Sceptre X505BV-FSR - 50 LED TV - 1080p" do
+        expect(@result["title"]).to eql("Sceptre X505BV-FSR - 50 LED TV - 1080p")
+      end
+
+      it "has a title" do
+        expect(@result["title"]).to_not be_empty
+      end
+
+      it "has a price" do
+        expect(@result["prices"]).to_not be_empty
+      end
+
+      it "has reviews" do
+        expect(@result["reviews"]).to be_an(Integer)
+      end
+
+      it "has a rating" do
+        expect(@result["rating"]).to be_an(Integer)
+      end
+      
+      it "has extensions" do
+        expect(@result["extensions"]).to_not be_empty
+      end
+
+      it "has a description" do
+        expect(@result["description"]).to_not be_empty
+      end
+      
+      it "has media" do
+        expect(@result["media"]).to_not be_empty
+      end
+
+    end
+
+  end
+
+end
